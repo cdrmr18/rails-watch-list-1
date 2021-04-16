3505bf9f1191e989b9a8034b793dbea6473ed3b2
Kathy Tavia <kathy.tavia@gmail.com>
2021-04-16 23:10:32 +0200

    completed adding validations and associations to models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
     has_many :bookmarks
-    has_many :reviews
+
+    validates :title, presence: true, uniqueness: true
+    validates :overview, presence: true
 end
