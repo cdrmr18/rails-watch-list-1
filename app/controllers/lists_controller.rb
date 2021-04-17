40d23763d2b68fb4b5d7c6bcc6cdfd7afebcb88f
Kathy Tavia <kathy.tavia@gmail.com>
2021-04-17 18:16:43 +0200

    completed list controller methods

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,17 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
     @movies = @list.movies
   end
 
   def new
-    @list = List.new()
+    @list = List.new
   end
 
   def create
@@ -22,7 +24,16 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name, :comment)
