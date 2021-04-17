985a8bd27da908f229b88b59016f7355b6c0c5e6
Kathy Tavia <kathy.tavia@gmail.com>
2021-04-17 15:38:21 +0200

    added navbar and completed methofs fot list controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,8 @@
 class BookmarksController < ApplicationController
   def create
+    @list = List.new(params[:id])
+    @movie = Movie.new()
+    @bookmark = Bookmark.new(@list,  @movie)
   end
 
   def destroy
